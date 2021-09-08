72d2eece28ef872e5a222841050967a6bba3613b
PerreeMatias <MatiasPerree@users.noreply.github.com>
2021-09-08 12:46:35 -0300

    Update Main.java

--- core/src/com/garaperree/guazo/Main.java
+++ core/src/com/garaperree/guazo/Main.java
@@ -2,24 +2,30 @@ package com.garaperree.guazo;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Sprite spr;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		spr = new Sprite(img);
+		spr.setX(100);
+		spr.setY(100);
+		spr.setSize(300, 300);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(spr, spr.getX(),spr.getY(), 500, 500);
 		batch.end();
 	}
 	
