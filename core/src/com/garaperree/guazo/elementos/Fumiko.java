52286f5dd2e78ddc86d515439becf49d35199792
PerreeMatias <MatiasPerree@users.noreply.github.com>
2021-10-08 10:49:48 -0300

    Update Fumiko.java

--- core/src/com/garaperree/guazo/elementos/Fumiko.java
+++ core/src/com/garaperree/guazo/elementos/Fumiko.java
@@ -1,5 +1,7 @@
 package com.garaperree.guazo.elementos;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,6 +12,8 @@ public class Fumiko {
 		public Sprite spr;
 		public float alto,ancho;
 		public float x,y;
+		//codigo de miche
+		float personajeSpeed = 10.0f;
 		
 		public Fumiko(float x, float y, float ancho, float alto) {
 			textura = new Texture("Fumiko.jpg");
@@ -22,6 +26,23 @@ public class Fumiko {
 
 		public void dibujar(SpriteBatch batch) {
 			spr.draw(batch);
+		}
+		
+		public void Render() {
+			//codigo de miche
+			if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT)) {
+	              x -= Gdx.graphics.getDeltaTime() * personajeSpeed;
+	              System.out.println("Izquierda");
+	         }else if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT)) {
+	              x += Gdx.graphics.getDeltaTime() * personajeSpeed;
+	              System.out.println("Derecha");
+	         }else if(Gdx.input.isKeyPressed(Keys.DPAD_UP)) {
+	              y += Gdx.graphics.getDeltaTime() * personajeSpeed;
+	              System.out.println("Arriba");
+	         }else if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN)) {
+	              y -= Gdx.graphics.getDeltaTime() * personajeSpeed;
+	              System.out.println("Abajo");
+	           }
 		}
 	}
 	
